503fadbc8b27d2dd5358ad50adc8a14ceaa88390
PC-24 <mohamedzbedat77@gmail.com>
2025-11-12 11:39:04 +0200

    r1234ed2323

--- app/src/main/java/com/example/random/MainActivity.java
+++ app/src/main/java/com/example/random/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.random;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
@@ -13,13 +14,16 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button start, new1;
+    private Button start, new1, btnScore;
     private TextView tvnum, tvnum2, tvnum3, tvnum4, tvnum5, tvnum6, tvnum7, tvnum8;
     private int a1, a2, a3, a4, a5, a6, n = 0 , x=0;
     private boolean isRunning = false;
     private Handler handler = new Handler();
     private Random rand = new Random();
     private Runnable runnable;
+    private Intent score;
+    public static int game=0 ;
+    public static int correct=0 ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,9 +39,12 @@ public class MainActivity extends AppCompatActivity {
         tvnum6 = findViewById(R.id.textView6);
         tvnum7 = findViewById(R.id.textViewR);
         tvnum8 = findViewById(R.id.textViewOf);
+        btnScore = findViewById(R.id.button3);
+        score = new Intent(MainActivity.this, ScoreActivity.class);
 
 
         new1.setOnClickListener(v -> {
+            game++;
             a1 = rand.nextInt(40);
             tvnum.setText(String.valueOf(a1));
             a2 = rand.nextInt(40);
@@ -57,6 +64,16 @@ public class MainActivity extends AppCompatActivity {
             tvnum5.setBackgroundColor(0);
             tvnum6.setBackgroundColor(0);
 
+            runnable = new Runnable() {
+                @Override
+                public void run() {
+                    int number = rand.nextInt(39) + 1;
+                    tvnum7.setText(String.valueOf(number));
+                    handler.postDelayed(runnable, 100);
+
+                }
+            };
+
             tvnum8.setText("0 of 6");
             n = 0;
 
@@ -74,19 +91,11 @@ public class MainActivity extends AppCompatActivity {
                 start.setText("stop");
                 start.setBackgroundColor(Color.RED);
 
-                runnable = new Runnable() {
-                    @Override
-                    public void run() {
-                        int number = rand.nextInt(39) + 1;
-                        tvnum7.setText(String.valueOf(number));
 
-                        if (isRunning)
-                            handler.postDelayed(this, 100);
-                    }
-                };
                 handler.post(runnable);
 
-            } else {
+                }
+            else {
 
                 isRunning = false;
                 handler.removeCallbacks(runnable);
@@ -100,61 +109,51 @@ public class MainActivity extends AppCompatActivity {
                 if (tvnum.getText().toString().equals(String.valueOf(current))) {
                     tvnum.setBackgroundColor(Color.RED);
                     n++;
+                    correct++;
+
                 }
                 if (tvnum2.getText().toString().equals(String.valueOf(current))) {
                     tvnum2.setBackgroundColor(Color.RED);
                     n++;
+                    correct++;
+
                 }
                 if (tvnum3.getText().toString().equals(String.valueOf(current))) {
                     tvnum3.setBackgroundColor(Color.RED);
                     n++;
+                    correct++;
+
                 }
                 if (tvnum4.getText().toString().equals(String.valueOf(current))) {
                     tvnum4.setBackgroundColor(Color.RED);
                     n++;
+                    correct++;
+
                 }
                 if (tvnum5.getText().toString().equals(String.valueOf(current))) {
                     tvnum5.setBackgroundColor(Color.RED);
                     n++;
+                    correct++;
+
                 }
                 if (tvnum6.getText().toString().equals(String.valueOf(current))) {
                     tvnum6.setBackgroundColor(Color.RED);
-
+                    correct++;
                     n++;
                 }
 
 
                 tvnum8.setText(n + " of 6");
+
+            }
+        });
+
+        btnScore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                startActivity(score);
             }
         });
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
---
+++ app/src/main/java/com/example/random/ScoreActivity.java
@@ -0,0 +1,43 @@
+package com.example.random;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ScoreActivity extends AppCompatActivity {
+    private TextView tvnum10, tvnum11;
+    private Button btnBack;
+    private Intent intent;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_score);
+        tvnum10 = findViewById(R.id.textView7);
+        tvnum11= findViewById(R.id.textView8);
+        btnBack = findViewById(R.id.button4);
+        intent = new Intent(ScoreActivity.this, MainActivity.class);
+        tvnum10.setText("Num of games :"+MainActivity.game);
+        tvnum11.setText("Num of games :"+MainActivity.correct);
+
+
+
+
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                startActivity(intent);
+            }
+        });
+
+    }
+}
